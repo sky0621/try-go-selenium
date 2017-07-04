241568957f614acab23eaaad6999ff63468aac9a
sky0621 <sky14work@gmail.com>
2017-07-05 02:02:17 +0900

    try

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/labstack/gommon/log"
+	"github.com/sclevine/agouti"
+)
+
+func main() {
+	driver := agouti.ChromeDriver()
+	if err := driver.Start(); err != nil {
+		log.Fatalf("driver start failed %#v", err)
+	}
+	defer driver.Stop()
+
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	if err != nil {
+		log.Fatalf("new page failed %#v", err)
+	}
+
+	if err := page.Navigate("http://qiita.com/"); err != nil {
+		log.Fatalf("Failed to navigate:%v", err)
+	}
+	err = page.Screenshot("./chrome_qiita.jpg")
+	if err != nil {
+		log.Fatalf("page screenshot failed %#v", err)
+	}
+}
